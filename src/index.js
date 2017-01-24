a07d6673f2f0e8498498083b9d85fc29efbdfcf7
Aubrian Halili <aubrian.halili.ags@gmail.com>
2017-01-25 02:45:56 +0800

    Utilized redux-storage and redux-storage-engine-localstorage

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { browserHistory } from 'react-router';
+import * as storage from 'redux-storage';
+import createEngine from 'redux-storage-engine-localstorage';
 
-import shoutApp from './reducers';
+import reducers from './reducers';
 import Root from './components/Root';
+
+const engine = createEngine('YjGQoDeJHdAm');
+const storageMiddleware = storage.createMiddleware(engine);
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
-  shoutApp,
+  storage.reducer(reducers),
   composeEnhancers(
-    applyMiddleware(thunk),
+    applyMiddleware(thunk, storageMiddleware),
   ),
 );
+
+const load = storage.createLoader(engine);
+load(store);
 
 render(
   <Provider store={store}>
