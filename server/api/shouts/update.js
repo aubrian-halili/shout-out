000e99d936205eb57247f32c78d0b7190f88b2e5
Aubrian Halili <aubrian.halili.ags@gmail.com>
2017-01-28 00:46:42 +0800

    Fixed the issue with updated_at that is not updating

--- server/api/shouts/update.js
+++ server/api/shouts/update.js
@@ -4,8 +4,13 @@ import { success, badRequest, error } from '../../util/response';
 
 const update = (req, res) => {
   const id = req.params.shoutId;
-  const text = req.body.text;
-  Shout.findByIdAndUpdate(id, { text }, (err, shout) => {
+  const currentDate = new Date();
+
+  const values = {
+    text: req.body.text,
+    updated_at: currentDate,
+  };
+  Shout.findByIdAndUpdate(id, values, (err, shout) => {
     if (err) {
       error(res, 'Something went wrong');
     } else if (_.isNull(shout)) {
